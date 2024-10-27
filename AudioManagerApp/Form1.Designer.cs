b262f2492d2a08bb4eb68d3b06f7274335d573cb
RaghavArora2 <raghavarora419@gmail.com>
2024-10-27 21:04:04 +0530

    Add project files.

---
+++ AudioManagerApp/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace AudioManagerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.deviceComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.appListBox = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(240, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Output Device:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // deviceComboBox
+            // 
+            this.deviceComboBox.AccessibleName = "deviceComboBox";
+            this.deviceComboBox.FormattingEnabled = true;
+            this.deviceComboBox.Location = new System.Drawing.Point(276, 83);
+            this.deviceComboBox.Name = "deviceComboBox";
+            this.deviceComboBox.Size = new System.Drawing.Size(190, 24);
+            this.deviceComboBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(178, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(386, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select Applications to Route to Device:";
+            // 
+            // appListBox
+            // 
+            this.appListBox.AccessibleName = "appListBox";
+            this.appListBox.FormattingEnabled = true;
+            this.appListBox.Location = new System.Drawing.Point(245, 217);
+            this.appListBox.Name = "appListBox";
+            this.appListBox.Size = new System.Drawing.Size(286, 174);
+            this.appListBox.TabIndex = 3;
+            this.appListBox.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "applyButton";
+            this.button1.Location = new System.Drawing.Point(650, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Apply Settings";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "saveProfileButton";
+            this.button2.Location = new System.Drawing.Point(650, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 46);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Save Profile";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.appListBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.deviceComboBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox deviceComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox appListBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AudioManagerApp/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
+
+namespace AudioManagerApp
+{
+    public partial class Form1 : Form
+    {
+        private AudioSessionManager audioSessionManager;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(this.Form1_Load);
+            audioSessionManager = new AudioSessionManager();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadAudioDevices();
+            LoadRunningApplications();
+        }
+
+        private void LoadAudioDevices()
+        {
+            var deviceEnumerator = new MMDeviceEnumerator();
+            var devices = deviceEnumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
+
+            foreach (var device in devices)
+            {
+                deviceComboBox.Items.Add(device.FriendlyName);
+            }
+
+            if (deviceComboBox.Items.Count > 0)
+            {
+                deviceComboBox.SelectedIndex = 0; // Select the first device by default
+            }
+        }
+
+        private void LoadRunningApplications()
+        {
+            Process[] processList = Process.GetProcesses();
+            foreach (Process process in processList)
+            {
+                if (!string.IsNullOrEmpty(process.MainWindowTitle) && !appListBox.Items.Contains(process.MainWindowTitle))
+                {
+                    appListBox.Items.Add(process.MainWindowTitle);
+                }
+            }
+        }
+
+        private void applyButton_Click(object sender, EventArgs e)
+        {
+            string selectedDevice = deviceComboBox.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(selectedDevice))
+            {
+                MessageBox.Show("Please select an output device.");
+                return;
+            }
+
+            foreach (var item in appListBox.CheckedItems)
+            {
+                RouteAudioToDevice(item.ToString(), selectedDevice);
+            }
+        }
+
+        private void RouteAudioToDevice(string applicationName, string deviceName)
+        {
+            var session = audioSessionManager.GetSessionByProcessName(applicationName);
+
+            if (session != null)
+            {
+                var deviceEnumerator = new MMDeviceEnumerator();
+                var devices = deviceEnumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
+                var targetDevice = devices.FirstOrDefault(d => d.FriendlyName.Equals(deviceName, StringComparison.OrdinalIgnoreCase));
+
+                if (targetDevice != null)
+                {
+                    // Obtain the session control correctly
+                    var sessionControl = session.GetSessionControl();
+
+                    if (sessionControl != null)
+                    {
+                        // Use the session control to adjust the audio output
+                        // This might require additional steps depending on how you implement audio routing
+                        // Here we simply inform the user
+                        MessageBox.Show($"Routing '{applicationName}' to '{deviceName}'");
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Audio session control not found for '{applicationName}'.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Device '{deviceName}' not found.");
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Application '{applicationName}' not found.");
+            }
+        }
+
+        private void saveProfileButton_Click(object sender, EventArgs e)
+        {
+            var profile = new Profile
+            {
+                SelectedDevice = deviceComboBox.SelectedItem?.ToString(),
+                Applications = appListBox.CheckedItems.Cast<string>().ToList()
+            };
+
+            SaveProfile(profile);
+        }
+
+        private void SaveProfile(Profile profile)
+        {
+            MessageBox.Show("Profile saved!");
+        }
+
+        // Dummy handlers for missing event declarations
+        private void label1_Click(object sender, EventArgs e) { }
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void button2_Click(object sender, EventArgs e) { }
+    }
+
+    public class Profile
+    {
+        public string SelectedDevice { get; set; }
+        public List<string> Applications { get; set; }
+    }
+
+    public class AudioSession
+    {
+        public string ProcessName { get; set; }
+        public int SessionId { get; set; }
+        public IAudioSessionControl sessionControl;
+
+        public AudioSession(IAudioSessionControl sessionControl)
+        {
+            this.sessionControl = sessionControl;
+
+            // Try to parse the session identifier to an int
+            // Assuming GetSessionIdentifier returns a string representation of the session ID
+            if (int.TryParse(sessionControl.GetSessionIdentifier(), out int sessionId)) // Ensure GetSessionIdentifier is a method
+            {
+                this.SessionId = sessionId; // Assign the parsed integer
+            }
+            else
+            {
+                MessageBox.Show("Failed to parse session identifier to integer.");
+            }
+        }
+
+        public IAudioSessionControl GetSessionControl()
+        {
+            return sessionControl; // Return the control directly
+        }
+    }
+
+    public class AudioSessionManager
+    {
+        private readonly List<AudioSession> sessions;
+
+        public AudioSessionManager()
+        {
+            sessions = new List<AudioSession>();
+            LoadAudioSessions();
+        }
+
+        private void LoadAudioSessions()
+        {
+            var deviceEnumerator = new MMDeviceEnumerator();
+            var device = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            var sessionManager = device.AudioSessionManager;
+
+            for (int i = 0; i < sessionManager.Sessions.Count; i++)
+            {
+                var session = sessionManager.Sessions[i];
+                var audioSession = new AudioSession(session); // Pass the session control
+                audioSession.ProcessName = session.GetSessionIdentifier; // Assuming this is a method
+                sessions.Add(audioSession);
+            }
+        }
+
+        public AudioSession GetSessionByProcessName(string processName)
+        {
+            return sessions.FirstOrDefault(session => session.ProcessName.Equals(processName, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
